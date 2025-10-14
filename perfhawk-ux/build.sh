8ee99bdbe6a881e81078f015a1cc98f89f4221e2
Prasath <prasath.mahendraraja@gmail.com>
2025-10-14 14:04:02 -0400

    Added perfhawk-ux base image and steps

---
+++ perfhawk-ux/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Accept Node.js version as an argument 
+if [ -z "$1" ]; then
+  echo "Node.js version is required."
+  exit 1
+fi
+NODE_VERSION=$1
+
+# Build the Docker image with the specified Node.js version
+docker build --build-arg NODE_VERSION=$NODE_VERSION -t prasathmahendraraja/node:$NODE_VERSION-alpine-python3-sqlite3-chromedriver-git .
