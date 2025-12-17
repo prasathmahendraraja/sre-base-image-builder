df862d3b6e22417616df61590978ec3e0bf76e0e
Prasath <prasath.mahendraraja@gmail.com>
2025-12-17 10:14:18 -0500

    added base images for excel worker

---
+++ dotnet6-node16-edge-ubuntu20/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build the Docker image
+docker build -t prasathmahendraraja/dotnet6-node16-edge-ubuntu20 .
---
+++ phantomjs-ubuntu-2.5.0/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build the Docker image
+docker build -t prasathmahendraraja/phantomjs-ubuntu:2.5.0 .
